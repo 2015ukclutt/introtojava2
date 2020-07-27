c7554a973c2b2d4e8c8c63657894f32bbddcb4a8
2015ukclutt <55856496+2015ukclutt@users.noreply.github.com>
2020-07-27 15:46:17 +0100

    Updates

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 /*
- * Below are four exercises which perform different tasks. Each one is missing lines of code, indicated by _____________ . Fill in as many of
- * these blanks as you can until your program runs correctly with no errors
+ * Below are four exercises which perform different tasks. Each one is missing lines of code, indicated by _____________ . 
+ * 
+ * Follow the instructions as given in the slides
+ * Full solutions to these tasks will be uploaded to the github repository after the session
  */
 
 import java.util.*;
@@ -16,34 +18,22 @@ public class Main
 		exercise4();
 	}
 	public static void exercise1() {
-		/*Exercise 1
-		 * Prompt the user to enter an integer. Check whether the integer is odd or even.
-		 * If EVEN: Output "Value is even" where 'value' is replaced by the integer which has been entered 
-		 * If ODD: Output "Value is odd" where 'value' is replaced by the integer which has been entered
+		/*
+		 * This program has an array of fruits, which must be outputted individually to the console in the format: "Fruit 1 : apple"
 		 */
-		System.out.println("Please enter an integer");
 		
-		Scanner userInput = new ______________
-		int integerInput = userInput.___________
-		System.out.println("The number you have entered is: " + ______________);
-		
-		if (integerInput % 2 == 0) {
-			________________________
-		}
-		else if (integerInput % 2 == 1) {
-			________________________
-		}
-		else {
-			System.out.println("Invalid value entered");
+		String[] fruits = {"apple", "banana", "pear", "strawberry", "raspberry", "pomegranate", "orange"};
+		for (int i = ____; i __ fruits.length; ____) {
+			System.out.println("Fruit " + (i+1) + " = " + fruits[__]);
 		}
 	}
 	
 	public static void exercise2() {
-		/*Exercise 2
+		/*
 		 * Prompt the user to enter three integers
 		 * Output the largest of the three values
 		 */
-		
+	
 		Scanner scanner = new ________________
 				
 		System.out.println("Enter first number:");
@@ -65,20 +55,34 @@ public class Main
 	}
 	
 	public static void exercise3() {
-		/*Exercise 3
-		 * This program has an array of fruits, which must be outputted individually to the console in the format: "Fruit 1 : apple"
+		/*
+		 * Prompt the user to enter an integer. Check whether the integer is odd or even.
+		 * If EVEN: Output "Value is even" where 'value' is replaced by the integer which has been entered 
+		 * If ODD: Output "Value is odd" where 'value' is replaced by the integer which has been entered
 		 */
 		
-		__________ fruits = {"apple", "banana", "pear", "strawberry", "raspberry", "pomegranate", "orange"};
-		for (_________; i < fruits.length; ____) {
-			System.out.println("Fruit " + (i+1) + " = " + fruits[__]);
+		System.out.println("Please enter an integer");
+		
+		Scanner userInput = new ______________
+		int integerInput = userInput.___________
+		System.out.println("The number you have entered is: " + ______________);
+		
+		if (integerInput % 2 == 0) {
+			________________________
+		}
+		else if (integerInput % 2 == 1) {
+			________________________
+		}
+		else {
+			System.out.println("Invalid value entered");
 		}
 	}
 	
 	public static void exercise4() {
-		/*Exercise 4
+		/*
 		 * Create a method which takes a string from the user and outputs the reverse of the string
 		 */
+		
 		________ inputString = new Scanner(System.in);
 		System.out.println("Please enter a word");
 		______ str = inputString.next();
@@ -90,9 +94,4 @@ public class Main
 		____________________________
 		____________.close();
 	}
-		
-		
-		
-	
-
 }
